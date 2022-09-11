9951edea2a1239c6189c320f6da899459d075e9d
Crizel <rhzhzh3@naver.com>
2022-09-11 17:19:31 +0900

    Add files via upload

---
+++ js/pages.js
@@ -0,0 +1,42 @@
+function fn_htmls(page){
+    var html = "";
+    if(page == 'main'){             
+        html = `
+        <div class="mainImg">
+            <img src="./images/common/main01.png">
+        </div>
+    `;       
+    }else if(page == 'intro'){
+        html += `
+            <h3>준비중입니다.</h3>
+        `;
+    }else if(page == 'location'){
+        html += `
+            <table class="tbl_type01">
+                <colgroup>
+                    <col style="width:30%;"/>
+                    <col style="width:70%;"/>
+                </colgroup>
+                <tr>
+                    <th>주소</th>
+                    <td class="left"><a href="http://kko.to/0sBrLQzQU" target="_blank">경남 진주시 가좌동 1640</a></td>
+                </tr>
+                <tr>
+                    <th>연락처</th>
+                    <td class="left"><a href="tel:055-757-8266">055-757-8266</a></td>
+                </tr>        
+            </table>
+
+            <div id="map"></div>            
+        `;
+
+    }else if(page == 'work'){
+        html += `
+            <h3>준비중입니다.</h3>
+        `;
+    }    
+
+
+    return html;
+}
+
