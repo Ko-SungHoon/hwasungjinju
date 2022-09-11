0d87f7e65af7eae6e14f2ba35cca85ab8de344f8
Crizel <rhzhzh3@naver.com>
2022-09-11 17:44:18 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -2,16 +2,11 @@ function fn_getPage(page){
     var html = fn_htmls(page);
     var titleStr = "";
 
-    if(page == 'main'){             titleStr = ""           }
-    else if(page == 'intro'){       titleStr = "회사소개"   }
-    else if(page == 'location'){    titleStr = "오시는길"       }
-    else if(page == 'work'){        titleStr = "업무소개"       }    
+    if(page == 'main'){             titleStr = "" ;         document.querySelector("#map").style.display = "none";      }
+    else if(page == 'intro'){       titleStr = "회사소개";   document.querySelector("#map").style.display = "none";     }
+    else if(page == 'location'){    titleStr = "오시는길";   document.querySelector("#map").style.display = "block";    }
+    else if(page == 'work'){        titleStr = "업무소개";   document.querySelector("#map").style.display = "none";    }    
 
     document.querySelector("#titleStr").innerHTML = titleStr;
     document.querySelector(".content").innerHTML = html;
-
-    if(page == 'location'){
-          
-    }
-
 }
